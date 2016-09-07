ede5666ec327a124bf33fec554475c3cd7b1b65f
kobake <kobake@users.sourceforge.net>
2016-09-08 01:53:54 +0900

    Create ASP.NET MVC Project

---
+++ BbsSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BbsSample.MvcApplication" Language="C#" %>
---
+++ BbsSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BbsSample.Startup))]
+namespace BbsSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
