79edde8d6cae7c0a448354129db4259ddca28f0f
kobake <kobake@users.sourceforge.net>
2016-09-08 03:30:09 +0900

    Comment本文を複数行テキストに変更

--- BbsSample/Models/Comment.cs
+++ BbsSample/Models/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,10 @@ namespace BbsSample.Models
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+
         public DateTime Created { get; set; }
     }
 }
