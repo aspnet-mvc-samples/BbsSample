f3b6f327327bc3ea82a8b021f9cf467601067f65
kobake <kobake@users.sourceforge.net>
2016-09-08 03:19:53 +0900

    デフォルトルートを Comments.Index に設定

--- BbsSample/App_Start/RouteConfig.cs
+++ BbsSample/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BbsSample
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Comments", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
