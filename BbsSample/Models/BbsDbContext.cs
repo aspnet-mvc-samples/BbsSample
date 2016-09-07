edc65c41ce115833c8b57d616e1fc34a60af8d4c
kobake <kobake@users.sourceforge.net>
2016-09-08 02:59:34 +0900

    BbsDbContext

---
+++ BbsSample/Models/BbsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BbsSample.Models
+{
+    public class BbsDbContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
