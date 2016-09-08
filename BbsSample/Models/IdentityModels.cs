c82817912ac891cbbec8df72f777dfbef71da8e9
kobake <kobake@users.sourceforge.net>
2016-09-09 00:51:00 +0900

    ApplicationUser に TwitterAccount 追加

--- BbsSample/Models/IdentityModels.cs
+++ BbsSample/Models/IdentityModels.cs
@@ -16,6 +16,8 @@ namespace BbsSample.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string TwitterAccount { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
