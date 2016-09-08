b66f62239f8eeba6942228dd5826f702819899ec
kobake <kobake@users.sourceforge.net>
2016-09-08 20:40:23 +0900

    全体的にページ内容調整

--- BbsSample/Controllers/HomeController.cs
+++ BbsSample/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace BbsSample.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
